1ac377f2302fc1b79ea9df509ad8d575e6045747
Trong. Nguyen Quoc - CMC Global DU3.11 <nqtrong@cmcglobal.vn>
2022-05-09 10:13:21 +0700

    [Training] Setup application, repository, wrapper, mapping

---
+++ Template.Training.Mvc/Core.Application/Helpers/PaginationHelper.cs
@@ -0,0 +1,31 @@
+﻿using Core.Application.Interfaces;
+using Core.Application.Parameters;
+using Core.Application.Wrappers;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Application.Helpers
+{
+    public class PaginationHelper
+    {
+        public static PagedResponse<List<T>> CreatePagedReponse<T>(List<T> pagedData, RequestParameter validFilter, int totalRecords, IUriService uriService, string route)
+        {
+            var respose = new PagedResponse<List<T>>(pagedData, validFilter.PageNumber, validFilter.PageSize);
+            var totalPages = ((double)totalRecords / (double)validFilter.PageSize);
+            int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
+            respose.NextPage =
+                validFilter.PageNumber >= 1 && validFilter.PageNumber < roundedTotalPages
+                ? uriService.GetPageUri(new RequestParameter(validFilter.PageNumber + 1, validFilter.PageSize), route)
+                : null;
+            respose.PreviousPage =
+                validFilter.PageNumber - 1 >= 1 && validFilter.PageNumber <= roundedTotalPages
+                ? uriService.GetPageUri(new RequestParameter(validFilter.PageNumber - 1, validFilter.PageSize), route)
+                : null;
+            respose.FirstPage = uriService.GetPageUri(new RequestParameter(1, validFilter.PageSize), route);
+            respose.LastPage = uriService.GetPageUri(new RequestParameter(roundedTotalPages, validFilter.PageSize), route);
+            respose.TotalPages = roundedTotalPages;
+            respose.TotalRecords = totalRecords;
+            return respose;
+        }
+    }
+}
